7bfe93e47ee1bdb7681f83206728ccca616f626d
Sebastien Blanc <scm.blanc@gmail.com>
2021-05-29 16:35:42 +0200

    Update NutritionFacts.java

--- src/effectivejava/chapter2/item2/builder/NutritionFacts.java
+++ src/effectivejava/chapter2/item2/builder/NutritionFacts.java
@@ -51,5 +51,6 @@ public class NutritionFacts {
     public static void main(String[] args) {
         NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
                 .calories(100).sodium(35).carbohydrate(27).build();
+        System.out.println(cocaCola);
     }
 }
